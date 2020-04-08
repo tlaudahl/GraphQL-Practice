d96554f10cb2a95935395ea102448798cbb597c0
tlaudahl <travis.laudahl@gmail.com>
2020-04-08 00:39:40 -0500

    Added create user schema

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
 const Card = require('./models/card');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,11 +23,22 @@ app.use(
                 date: String!
             }
 
+            type User {
+                _id: ID!
+                email: String!
+                password: String
+            }
+
             input CardInput {
                 title: String!
                 question: String!
                 answer: String!
                 date: String!
+            }
+
+            input UserInput {
+                email: String!
+                password: String!
             }
 
             type RootQuery {
@@ -34,6 +47,7 @@ app.use(
 
             type RootMutation {
                 createCard(cardInput: CardInput): Card
+                createUser(userInput: UserInput): User
             }
 
             schema {
@@ -56,15 +70,47 @@ app.use(
                     title: args.cardInput.title,
                     question: args.cardInput.question,
                     answer: args.cardInput.answer,
-                    date: new Date(args.cardInput.date)
+                    date: new Date(args.cardInput.date),
+                    creator: '5e8d60c59081d2497c2f39b2'
                 });
+                let createdEvent;
                 return card.save()
                 .then(result => {
-                    return {...result._doc};
+                    createdEvent = { ...result._doc, _id: card.id };
+                    return User.findById('5e8d60c59081d2497c2f39b2')
+                })
+                .then(user => {
+                    if (!user) {
+                        throw new Error('User not found')
+                    }
+                    user.createdCards.push(card)
+                    return user.save();
+                })
+                .then(result => {
+                    return createdEvent;
                 })
                 .catch(err => {
                     throw err;
                 });
+            },
+            createUser: args => {
+                return User.findOne({email: args.userInput.email}).then(user => {
+                    if (user) {
+                        throw new Error('User with that email exists already')
+                    }
+                    return bcrypt.hash(args.userInput.password, 12)
+                })
+                .then(hashedPass => {
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPass
+                    });
+                    return user.save();
+                }).then(result => {
+                    return { ...result._doc, password: null, _id: result.id }
+                }).catch(err => {
+                    throw err
+                })
             }
         },
         graphiql: true
